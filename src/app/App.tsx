b73a496bfe6cd1b675e1fbd6f5123809d5c53f2e
GuzeevZhenya <shyst.evgeny@mail.ru>
2023-12-07 08:40:42 +0300

    done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import { Decks } from '../features/decks/Decks.tsx'
 import { GlobalError } from './GlobalError/GlobalError.tsx'
+import { useAppSelector } from './store.ts'
+import { LinearLoader } from '../common/components/Loader/LinearLoader.tsx'
 
 export const App = () => {
+  const appStatus = useAppSelector((state) => state.app.status)
   return (
     <div>
+      {appStatus === 'loading' && <LinearLoader />}
       <Decks />
       <GlobalError />
     </div>
